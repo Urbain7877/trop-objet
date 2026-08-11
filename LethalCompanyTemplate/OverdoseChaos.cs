cb351120a3a9ca67486a346260ea8f25238a688a
Urbain7877 <138565176+Urbain7877@users.noreply.github.com>
2026-08-11 18:01:48 +0200

    Update OverdoseChaos.cs

--- LethalCompanyTemplate/OverdoseChaos.cs
+++ LethalCompanyTemplate/OverdoseChaos.cs
@@ -1,13 +1,14 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using UnityEngine;
 
 namespace OverdoseChaos
 {
     [BepInPlugin(GUID, NAME, VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string GUID = "com.tonnom.overdosechaos";
+        public const string GUID = "com.urbain7877.overdosechaos";
         public const string NAME = "OverdoseChaos";
         public const string VERSION = "1.0.0";
 
@@ -17,10 +18,30 @@ namespace OverdoseChaos
         private void Awake()
         {
             Log = Logger;
-            Log.LogInfo($"Le mod {NAME} est bien chargé !");
+            Log.LogInfo($"Le mod {NAME} est bien chargé et prêt à foutre le bazar !");
 
-            // Applique les patchs Harmony si tu en as
+            // Applique tous les patchs Harmony écrits dans le mod
             harmony.PatchAll();
+        }
+    }
+
+    // Patch pour multiplier le chaos des objets et des entités à l'atterrissage
+    [HarmonyPatch(typeof(RoundManager))]
+    public class RoundManagerPatch
+    {
+        [HarmonyPatch("BeginRound")]
+        [HarmonyPostfix]
+        static void PostfixBeginRound()
+        {
+            Plugin.Log.LogInfo("=== ATTERRISSAGE : Lancement du protocole OverdoseChaos ! ===");
+
+            // 1. Ici, tu peux booster les paramètres globaux de la lune si le RoundManager le permet
+            if (RoundManager.Instance != null)
+            {
+                // Multiplie par exemple le nombre d'objets scrap à l'intérieur
+                RoundManager.Instance.scrapValueMultiplier *= 2.5f;
+                Plugin.Log.LogInfo("Valeur du loot multipliée par 2.5 !");
+            }
         }
     }
 }
